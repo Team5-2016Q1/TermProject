97d3ef7153e6381a9d37ecb54af624ca048e549b
edward <edward.fankhauser@mavs.uta.edu>
2016-03-02 13:27:13 -0600

    First

---
+++ app/src/main/java/team5/calendarproject/MainPage.java
@@ -0,0 +1,14 @@
+package team5.calendarproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Android Studio Comment Time.
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
