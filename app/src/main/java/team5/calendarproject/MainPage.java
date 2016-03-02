d660c95bea1c894bb446376ca4681c0e3c25445f
edward <edward.fankhauser@mavs.uta.edu>
2016-03-02 13:29:38 -0600

    Try

--- app/src/main/java/team5/calendarproject/MainPage.java
+++ app/src/main/java/team5/calendarproject/MainPage.java
@@ -3,7 +3,7 @@ package team5.calendarproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Android Studio Comment Time.
+//Android Studio Comment
 public class MainPage extends AppCompatActivity {
 
     @Override
