4e36e4d7627080cc194d9cffd88c6142f8106cd0
edward <edward.fankhauser@mavs.uta.edu>
2016-03-27 18:13:31 -0500

    First revision of calendar view's XML done-ish

--- app/src/main/java/team5/calendarproject/MonthViewController.java
+++ app/src/main/java/team5/calendarproject/MonthViewController.java
@@ -1,26 +1,24 @@
 package team5.calendarproject;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.text.Layout;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 //Android Studio Comment time.
-public class MainPage extends AppCompatActivity {
+public class MonthViewController extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.month_view);
     }
 
     LinearLayout week_1 = (LinearLayout) findViewById(R.id.monthly_week_1);
     LinearLayout week_2 = (LinearLayout) findViewById(R.id.monthly_week_2);
     LinearLayout week_3 = (LinearLayout) findViewById(R.id.monthly_week_3);
-    LinearLayout week_4 = (LinearLayout) findViewById(R.id.monthly_week_4);
+    View week_4 = (View) findViewById(R.id.monthly_week_4);
     LinearLayout week_5 = (LinearLayout) findViewById(R.id.monthly_week_5);
 
 }
