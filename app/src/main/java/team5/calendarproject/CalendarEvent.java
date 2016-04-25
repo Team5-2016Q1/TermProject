5937d983aaa69d26e16335d4cc702e4fe08898e7
edward <edward.fankhauser@mavs.uta.edu>
2016-04-25 12:58:31 -0500

    Adding extra constructors to event and cal event classes

--- app/src/main/java/team5/calendarproject/CalendarEvent.java
+++ app/src/main/java/team5/calendarproject/CalendarEvent.java
@@ -9,6 +9,7 @@ public class CalendarEvent extends Event {
     private ArrayList<String> participants;
     private String location;
     private int endTime;
+    private int ends_hours_later;
     private int repeats;
     private boolean secondAlarm;
     private boolean thirdAlarm;
@@ -23,6 +24,28 @@ public class CalendarEvent extends Event {
         thirdAlarm = alarm3;
         //this.repeats = repeats;
     }
+
+
+    public CalendarEvent(int idNumber, int startHour, int startMinute, int startDay, int startMonth, int startYear,
+                         int hours_later, String title, String color, Boolean alarm1, Boolean alarm2, Boolean alarm3,
+                         ArrayList<String> participants, String location, int repeats) {
+        super(startHour, startMinute, startDay, startMonth, startYear, title, color, alarm1, idNumber);
+        this.participants = participants;
+        this.ends_hours_later = hours_later;
+        this.location = location;
+        secondAlarm = alarm2;
+        thirdAlarm = alarm3;
+        //this.repeats = repeats;
+    }
+
+    private void setEnds_hours_later(int totalHours) {
+        this.ends_hours_later = totalHours;
+    }
+
+    private int getEnds_hours_later() {
+        return ends_hours_later;
+    }
+
 
     public ArrayList<String> getParticipants() {
         return participants;
--- app/src/main/java/team5/calendarproject/Event.java
+++ app/src/main/java/team5/calendarproject/Event.java
@@ -7,7 +7,12 @@ import java.io.Serializable;
  */
 abstract class Event  implements Serializable {
     private int time;
+    private int hour;
+    private int minute;
     private String date;
+    private int day;
+    private int month;
+    private int year;
     private String title;
     private String color;
     private boolean alarm;
@@ -20,6 +25,58 @@ abstract class Event  implements Serializable {
         this.color = color;
         this.alarm = alarm1;
         this.DB_ID_NUMBER = dbIDNumber;
+    }
+
+    public Event(int hour, int minute, int day, int month, int year, String title, String color, boolean alarm1, int dbIDNumber) {
+        this.hour = hour;
+        this.minute = minute;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.title = title;
+        this.color = color;
+        this.alarm = alarm1;
+        this.DB_ID_NUMBER = dbIDNumber;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public int getTime() {
